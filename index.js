a84ead9425d41c4c18fa662972fb533438f68f00
Karan-Bharti1 <karan.codes1@gmail.com>
2025-08-29 15:57:24 +0530

    Question 3

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ console.log(totalSum)
 const nums1 = [2, 3, 4];
 const product=nums1.reduce((acc,curr)=>acc*curr,1)
 console.log(product)
+
+//Given an array, calculate the average of a numeric array.
+const scores = [80, 90, 70, 100];
+// expected result: 85
+
+const numericAverage=scores.reduce((acc,curr)=>{
+    acc+=curr
+    return acc
+},0)/scores.length
+console.log(numericAverage)
